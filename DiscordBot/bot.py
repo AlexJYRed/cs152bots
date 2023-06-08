a4db3e7984c142778d5d80e00754bd411e6d2d61
Benji White <blwhite@stanford.edu>
2023-06-07 21:15:05 -0700

    Added ALL of Milestone 3

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -10,6 +10,7 @@ from report import Report
 from moderator import Moderator
 import pdb
 import openai
+import boto3
 
 # Set up logging to the console
 logger = logging.getLogger('discord')
@@ -27,6 +28,11 @@ with open(token_path) as f:
     tokens = json.load(f)
     discord_token = tokens['discord']
 
+    # Setup OpenAI keys
+    openai.organization = tokens['openai-org']
+    openai.api_key = tokens['openai-api-key']
+    
+
 
 class ModBot(discord.Client):
     def __init__(self): 
@@ -37,7 +43,9 @@ class ModBot(discord.Client):
         self.mod_channels = {} # Map from guild to the mod channel id for that guild
         self.moderator = Moderator()
         self.reports = {} # Map from user IDs to the state of their report
+        self.report_type = None
         self.last_report = None
+        self.bot_report_message = None
         self.channel = None
 
     async def on_ready(self):
@@ -75,11 +83,11 @@ class ModBot(discord.Client):
             return
 
         # Check if this message was sent in a server ("guild") or if it's a DM
-        # if message.guild:
-        #     await self.handle_channel_message(message)
-        # else:
-        #     await self.handle_dm(message)
-        await self.handle_dm(message)
+        if message.guild:
+            await self.handle_channel_message(message)
+        else:
+             await self.handle_dm(message)
+        #await self.handle_dm(message)
 
     async def handle_dm(self, message):
         # Handle a help message
@@ -108,15 +116,13 @@ class ModBot(discord.Client):
                 await message.channel.send(r)
             if self.reports[author_id].report_complete():
                 self.last_report = self.reports[author_id]
+                self.report_type = "manual"
                 self.last_author_id = author_id
 
-        # If the report is complete or cancelled, remove it from our map
+        # Send the first message to start the moderator workflow
         if self.last_report:
             print('report flow is complete')
-            # if author_id is not None:
-            #     self.last_report = self.reports[author_id]
-            # print(message.content[:6])
-            # if message.channel.id == mod_channel.id and message.content[:6] == Moderator.HANDLE_KEYWORD:
+   
             if self.last_report.need_handle():
                 print('handling the report')
                 reported_user =await self.fetch_user(int(self.last_report.report_message.author.id))
@@ -139,25 +145,152 @@ class ModBot(discord.Client):
                     self.last_author_id = None
                 # self.reports.pop(author_id)
 
-    # async def handle_channel_message(self, message):
-    #     # Only handle messages sent in the "group-#" channel
-    #     if not message.channel.name == f'group-{self.group_num}':
-    #         return
+    # Handles automated classification and the moderator handling process
+    async def handle_channel_message(self, message):
 
-    #     # Forward the message to the mod channel
-    #     mod_channel = self.mod_channels[message.guild.id]
-    #     await mod_channel.send(f'Forwarded message:\n{message.author.name}: "{message.content}"')
-    #     scores = self.eval_text(message.content)
-    #     await mod_channel.send(self.code_format(scores))
+        mod_channel = self.mod_channels[message.guild.id]
 
-    
-    def eval_text(self, message):
-        ''''
-        TODO: Once you know how you want to evaluate messages in your channel, 
-        insert your code here! This will primarily be used in Milestone 3. 
-        '''
-        return message
+        # Only handle messages sent in the "group-#" channel
+        if message.channel.name == f'group-{self.group_num}':
 
+            # Evaluate using GPT and Amazon Comprehend
+            gpt_model = "gpt-3.5-turbo"
+            dox = self.eval_dox(message.content, gpt_model)
+            pii_response = self.eval_pii(message.content)
+
+            # At least one of the classifiers needs to be triggered to send a bot report
+            if dox == "Threatening" or len(pii_response) > 0:
+
+                # Forward the message to the mod channel
+                response_message = "**Automatically evaluated the following message:**\n```" + str(message.author.name) + ": \"" + str(message.content) + "\"```"
+
+                # Append PII report to the message if it was detected
+                if len(pii_response) > 0:
+                    response_message += "Detected the following types of sensitive personal information with probabilities:\n```"
+                    for i in pii_response:
+                        response_message = response_message + i['Name'] + ": " + str(i['Score']) + "\n"
+                    response_message += "```"
+
+                # Append GPT threat report if ti happened
+                if dox == "Threatening":
+                    response_message = response_message + gpt_model + " indicates that the message potentially threatens a doxing attack\n"
+
+                # Prompt the mod to handle the report
+                response_message += "Type *report* to take action."
+
+                await mod_channel.send(response_message)
+                self.last_report = True
+                self.report_type = "automated"
+                self.bot_report_message = message
+
+        # Handle Moderator messages in the mod channel
+        elif message.channel.name == f'group-{self.group_num}-mod' and self.last_report:
+            
+            # Handle automated reports
+            if self.report_type == "automated":
+                # Call the moderator pipeline
+                res = await self.moderator.handle_bot_report(self.bot_report_message, message, self.bot_report_message.author, self.channel)
+
+                # if the user responds, send the message to the mod channel
+                if res is not None:
+                    for r in res:
+                        await mod_channel.send(r)
+
+                if await self.moderator.report_complete():
+                    await self.moderator.reset()
+                    self.report_type = None
+                    self.last_report = None
+
+            # Handle manual reports
+            elif self.report_type == "manual":
+                author_id = message.author.id
+
+                if self.last_report.need_handle():
+
+                    # Gather information
+                    reported_user =await self.fetch_user(int(self.last_report.report_message.author.id))
+                    user = await self.fetch_user(int(self.last_report.userID))
+                    res = await self.moderator.handle_report(report = self.last_report, message = message, user = user, reported_user = reported_user, channel= self.channel)
+
+                    # Forward responses to mod channel
+                    if res is not None:
+                        for r in res:                
+                            await mod_channel.send(r)
+
+                    # Reset the process once the report is done
+                    if await self.moderator.report_complete():
+                        await self.moderator.reset()
+                        self.last_report = None
+                        self.report_type = None
+                        self.reports.pop(self.last_author_id)
+                        self.last_author_id = None
+
+    # GPT doxing evaluation
+    def eval_dox(self, message, model):
+
+        response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+        {"role": "system", "content": "You are a content moderation system designed to detect threats of doxing. Classify each input as either Threatening or Not Threatening."},
+        {"role": "user", "content": "I know where you live"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know where you work"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "Your address is mine"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I'm excited for the party tonight! Can I have your address?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "Where do you live?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know you're at College University"},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "I know where you live and I'm going to kill you"},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "Your ip address is 1.1.1.1"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know your ip address"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "What is your ip address?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know that your name is John Doe"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "Hey, do you still live in this area? I'm looking to catch up sometime"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "Hey I'm doing some research on you and am gonna post a bunch of your personal info. Starting with location, place of work, parents home, sibling address, etc."},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "Your social security numer is 123-45-6789"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know your social security number. Give me money or else I will leak this information to the world."},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "I know you study at stanford"},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "Where do you go to school?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I know your name. You're dead to me."},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "What is your social security number?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": "I am going to kill you"},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "I know who you are. Send me $10000 or else I'll post your social security number publicly."},
+        {"role": "assistant", "content": "Threatening"},
+        {"role": "user", "content": "Hey there man! Can you give me the address of the party?"},
+        {"role": "assistant", "content": "Not Threatening"},
+        {"role": "user", "content": message}
+        ]
+        )
+
+        return response['choices'][0]['message']['content']
+
+    # AWS PII detection
+    def eval_pii(self, message):
+
+        # Needs AWS CLI Credentials to be configured beforehand
+        aws_comprehend = boto3.client('comprehend')
+
+        # Detect PII components in the message
+        response = aws_comprehend.contains_pii_entities(Text = message, LanguageCode = 'en')
+        return response['Labels']
     
     def code_format(self, text):
         ''''
@@ -165,7 +298,7 @@ class ModBot(discord.Client):
         evaluated, insert your code here for formatting the string to be 
         shown in the mod channel. 
         '''
-        return "Evaluated: '" + text+ "'"
+        return "GPT evaluated message as: '" + text+ "'"
 
 
 client = ModBot()
